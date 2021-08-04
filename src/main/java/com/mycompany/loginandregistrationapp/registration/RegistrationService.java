e4427c48f946367f4812b0e02856a8648a1dfdbb
Saleh Samman <s.nomad2005@gmail.com>
2021-08-04 16:35:43 +0300

    Create registration controller, service and request and secured endpoints

---
+++ src/main/java/com/mycompany/loginandregistrationapp/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.mycompany.loginandregistrationapp.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
